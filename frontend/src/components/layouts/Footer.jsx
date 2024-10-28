74031fb4b1b7d2db79ef7b226bc4dc988def3d64
Aditya Pate <101105693+aadityapate@users.noreply.github.com>
2024-10-28 09:47:08 +0530

    Add files via upload

---
+++ frontend/src/components/layouts/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <>
+      <footer>
+        <p className="text-center">
+            Food Delievery Website - 2024-25. All Rights Reserved by Aditya@WSA
+        </p>
+      </footer>
+    </>
+  );
+}
